1f822c4020747d490052173249327aa0bd73eec7
Ismael Marin <ismael.marin@gmail.com>
2026-08-26 20:47:24 -0600

    chore: reek config for probe timeout param and spec style

--- spec/lib/rails_ai_bridge/registry/context_provider_client_spec.rb
+++ spec/lib/rails_ai_bridge/registry/context_provider_client_spec.rb
@@ -441,6 +441,5 @@ RSpec.describe RailsAiBridge::Registry::ContextProviderClient do
       expect(result.error).to be_a(RailsAiBridge::Registry::TimeoutError)
       expect(fake_transport).to have_received(:close)
     end
-
   end
 end
